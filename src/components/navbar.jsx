d03c41887bf2d0e2709c62a62ecfc286f6733ab5
Alireza <alireza.nasiri.381@gmial.com>
2022-07-15 16:08:43 +0430

    navbar styles done

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -14,6 +14,11 @@ const Navbar = () => {
               <NavLink className={"navbar-link text-decoration-none mx-2 router-link " + (splitLocation[1] === "categories" ? "router-link-active" : "")} to="/categories">Categories</NavLink>
           </nav>
         </div>
+        <div className='nav-mid d-none d-sm-flex flex-column text-center justify-content-between align-items-center'>
+          <div className='navbar-logo mt-2'></div>
+          {/* <img className='navbar-logo mt-1' src={require('../styles/logos/navbar.JPG')} alt="" /> */}
+          <span className='title mt-1'>Tisa Bag</span>
+        </div>
         <div className="nav-right d-flex align-items-center">
           <NavLink className={"mx-2 text-decoration-none in-out-btn "  + (splitLocation[1] === "signup" ? "router-link-active" : "")} to="/signup">Sign up</NavLink>
           <NavLink className={"mx-2 text-decoration-none in-out-btn "  + (splitLocation[1] === "signin" ? "router-link-active" : "")} to="/signin">Sign in</NavLink>
